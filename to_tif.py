24e1649c72abb468467bb3b75a9204a61ca37374
Be-Zero <qth1203610904@gmail.com>
2022-01-20 18:50:21 +0800

    update README.md

--- to_tif.py
+++ to_tif.py
@@ -7,15 +7,15 @@ import glob
 
 def S2tif(filename):
     # 打开栅格数据集
-    print(filename)
+    # print(filename)
     root_ds = gdal.Open(filename)
-    print(type(root_ds))
+    # print(type(root_ds))
     # 返回结果是一个list，list中的每个元素是一个tuple，每个tuple中包含了对数据集的路径，元数据等的描述信息
     # tuple中的第一个元素描述的是数据子集的全路径
     ds_list = root_ds.GetSubDatasets()  # 获取子数据集。该数据以数据集形式存储且以子数据集形式组织
     visual_ds = gdal.Open(ds_list[0][0])  # 打开第1个数据子集的路径。ds_list有4个子集，内部前段是路径，后段是数据信息
-    print(visual_ds)
-    print(f'打开数据为：{ds_list[0][1]}')
+    # print(visual_ds)
+    # print(f'打开数据为：{ds_list[0][1]}')
     # print(f'投影信息：{visual_ds.GetProjection()}')
     # print(f'栅格波段数：{visual_ds.RasterCount}')
     # print(f'栅格列数（宽度）：{visual_ds.RasterXSize}')
@@ -43,10 +43,9 @@ def S2tif(filename):
 
 if __name__ == "__main__":
     from osgeo import gdal
-    SAFE_Path = (r'D:\Documentation\Project\Grassland ecology\data\Sentinel-2\Sentinel-2')
+    SAFE_Path = (r'D:\Documentation\Project\Grassland ecology\data\Sentinel-2\Sentinel-2\2')
     data_list = glob.glob(SAFE_Path + "\\*.SAFE")
 
-    # filename = ('E:\\RSDATA\\Sentinel2\\L2A\\S2A_MSIL2A_20210220T024731_N9999_R132_T51STA_20210306T024402.SAFE\\MTD_MSIL2A.xml')
     for i in range(len(data_list)):
         data_path = data_list[i]
         filename = data_path + "\\MTD_MSIL2A.xml"
